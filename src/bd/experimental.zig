c55adfcd3d80fc81e233140bc962c131fca5b3d7
Sirius902 <3645979-Sirius902@users.noreply.gitlab.com>
2023-12-19 19:48:29 -0800

    Range check syscall tables in findPotentialSyscalls

--- src/bd/experimental.zig
+++ src/bd/experimental.zig
@@ -4,7 +4,8 @@ const fmtInstruction = @import("instruction.zig").fmtInstruction;
 
 pub const StreamError = EndianStreamSource.ReadError || EndianStreamSource.SeekError;
 
-const syscall_table_len = 11;
+const syscall_tables_len = 11;
+const syscall_table_sizes = [syscall_tables_len]usize{ 105, 368, 98, 0, 59, 35, 72, 37, 9, 0, 60 };
 
 /// Stream should be at the search start position.
 pub fn findPotentialSyscalls(stream: *EndianStreamSource, writer: anytype) (StreamError || @TypeOf(writer).Error)!void {
@@ -26,7 +27,7 @@ pub fn findPotentialSyscalls(stream: *EndianStreamSource, writer: anytype) (Stre
         if (instruction) |i| {
             switch (i) {
                 .Syscall => |s| {
-                    if (s.table < syscall_table_len) {
+                    if (s.table < syscall_tables_len and s.index < syscall_table_sizes[s.table]) {
                         try writer.print("Potential syscall: {X:0>8}: {X:0>4} = {}\n", .{ decode_pos, code, fmtInstruction(i) });
                     }
                 },
